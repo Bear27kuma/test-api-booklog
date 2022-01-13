faea9a3f0330a22daec7b08735115209817db53d
Yuta Okuma <y.dream.nest@icloud.com>
2022-01-14 00:15:46 +0900

    読書ログ記録のAPIを作成

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ const express = require('express');
 const app = express();
 // ポート番号の定義
 const port = 3000;
+// リクエストボディ格納用変数
 
 // JSONを使えるようにする
 app.use(express.json());
+
+// 読書ログを記録する
+app.post('/booklog', (req, res) => {
+    const booklog = req.body;
+
+    if (!(booklog.name && booklog.text)) {
+        res.json({
+            'ok': false,
+            'error': 'Invalid Parameter'
+        });
+    }
+
+    res.json({
+        'ok': true,
+        'booklog': booklog
+    });
+});
 
 // Expressサーバーを起動する
 app.listen(port, () => {
